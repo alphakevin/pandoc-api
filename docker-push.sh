284468fd1ef4d47fda87fba9583bd5d1ca1c6895
alphakevin <get.so.smart@gmail.com>
2019-08-14 15:46:01 +0800

    Fix docker script in ci

--- docker-push.sh
+++ docker-push.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -ex
 
 DOCKER_REPO=${DOCKER_REPO-alphakevin/pandoc-api}
 GIT_BRANCH=${GIT_BRANCH-$(git symbolic-ref --short HEAD)}
 
-if [ $GIT_BRANCH == "master" ]; then
+if [ "$GIT_BRANCH" = "master" ]; then
   TAG=latest
 else
-  TAG=$GIT_BRANCH
+  TAG="$GIT_BRANCH"
 fi
 
-# echo "docker push $DOCKER_REPO:$TAG"
-docker push $DOCKER_REPO:$TAG
+docker push "$DOCKER_REPO:$TAG"
